599b6575a4fd0a2b0bf0ab937bdff2c927b4bb06
Yessenia Colin <mrs.ycolin@Yessenias-MacBook-Air.local>
2023-02-07 14:07:46 -0500

    removed npm file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var _ = require ('lodash');
+
+var array = [1,2,3,4,5,6,7]
+
+console.log('answer:', _.without(array, 3));
+
 let button = document.getElementById("enter");
 let input = document.getElementById("user-input");
 let ul = document.querySelector("ul");
